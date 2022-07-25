277f2cb0f90ac8e23edd128d283c920d7db5c5d8
amieldelatorre <amieljames.delatorre@gmail.com>
2022-07-26 10:56:33 +1200

    Working solution with readme explaining how to run and assumptions made and pdf spec

---
+++ BackendCodingExercise/Program.cs
@@ -0,0 +1,135 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Calculate Employee Montly Payslip");
+Console.WriteLine("Input format is (first name, last name, annual salary, super rate (%), pay period)");
+Console.WriteLine("Enter csv filename:");
+string? filename = Console.ReadLine();
+
+if (filename == null || filename.Trim() == "")
+{
+    Console.WriteLine("Error: You have not entered a filename!");
+    return;
+}
+
+var path = Path.Combine(Directory.GetCurrentDirectory(), filename);
+string filetype;
+
+try
+{
+    filetype = filename.Substring(filename.LastIndexOf("."));
+}
+catch (ArgumentOutOfRangeException)
+{
+    Console.WriteLine("Error: There was no \".\" character in the filename!");
+    return;
+}
+
+
+if (filetype != ".csv" || !File.Exists(path))
+{
+    Console.WriteLine("Error: You have not entered a csv file or the file does not exist!");
+    return;
+}
+
+
+Console.WriteLine("\nOutput format is (name, pay period, gross income, income tax, net income, super)\n");
+string[] lines = System.IO.File.ReadAllLines(path);
+List<string> results = new() { "name,payperiod,grossIncome,incomeTax,netIncome,super" };
+
+foreach (string line in lines)
+{
+    if (line.Trim() == "")
+        continue;
+
+    string personResult = ProcessLine(line);
+    results.Add(personResult);
+    Console.WriteLine(personResult);
+}
+
+CreateOutputFile(results);
+
+Console.ReadKey();
+
+
+static double CalculateIncomeTax(double annualSalary)
+{
+    double incomeTax = 0;
+
+    if (annualSalary > 180000)
+    {
+        incomeTax += (annualSalary - 180000) * 0.39;
+        annualSalary = 180000;
+    }
+
+    if (annualSalary >= 70000)
+    {
+        incomeTax += (annualSalary - 70000) * 0.33;
+        annualSalary = 70000;
+    }
+
+    if (annualSalary >= 48000)
+    {
+        incomeTax += (annualSalary - 48000) * 0.30;
+        annualSalary = 48000;
+    }
+
+    if (annualSalary >= 14000)
+    {
+        incomeTax += (annualSalary - 14000) * 0.175;
+        annualSalary = 14000;
+    }
+
+    if (annualSalary <= 14000)
+    {
+        incomeTax += annualSalary * 0.105;
+        annualSalary = 0;
+    }
+
+    return incomeTax / 12;
+}
+
+static string ProcessLine(string line)
+{
+    string[] personLine = line.Split(",");
+    string firstName = personLine[0];
+    string lastName = personLine[1];
+    double annualSalary = Double.Parse(personLine[2]);
+    int superRate = Int32.Parse(personLine[3].Substring(0, personLine[3].Length - 1));
+    string payPeriod = personLine[4];
+
+    double grossIncome = Math.Round(annualSalary / 12, 2);
+    double incomeTax = Math.Round(CalculateIncomeTax(annualSalary), 2);
+    double netIncome = grossIncome - incomeTax;
+    double super = Math.Round(grossIncome * superRate / 100, 2);
+
+    MonthEnum month = Enum.Parse<MonthEnum>(payPeriod, true);
+
+    int lastDay = DateTime.DaysInMonth(DateTime.Now.Year, (int)month);
+
+
+    return $"{firstName} {lastName},01 {month} - {lastDay} {month},{grossIncome:f2},{incomeTax:f2},{netIncome:f2},{super:f2}";
+}
+
+static void CreateOutputFile(List<string> results)
+{
+    Console.WriteLine("\nPlease wait, writing results to output file.");
+    var outputFilePath = Path.Combine(Directory.GetCurrentDirectory(), "Results.csv");
+    File.WriteAllLines(outputFilePath, results);
+    Console.WriteLine("Finished writing to Results.csv");
+}
+
+enum MonthEnum
+{
+    None,
+    January,
+    February,
+    March,
+    April,
+    May,
+    June,
+    July,
+    August,
+    September,
+    October,
+    November,
+    December
+}
---
+++ README.md
@@ -0,0 +1,38 @@
+# Backend Coding Exercise
+
+## How to run
+### Running through Developer Command prompt for VS 2022
+- Place the desired csv file into <em>datacom_20220725\BackendCodingExercise</em> 
+    - There is an example file there named <em>examplefile.csv</em>
+- The csv should follow the format <em>(first name, last name, annual salary, super rate (%), pay period)</em>
+- Open Developer Command prompt for VS 2022
+- Change directory to <em>../datacom_20220725/BackendCodingExercise</em> 
+- Enter command to run the program
+```
+dotnet run program.cs 
+```
+- Enter the filename of the csv 
+- The csv should be processed and an output should be shown in console as well as an ouput file named <em>results.csv</em> will be generated
+
+### Running through Visual Studio Debug
+- Place the desired csv file into <em>datacom_20220725\BackendCodingExercise\bin\Debug\net6.0</em> 
+    - There is an example file there named <em>examplefile.csv</em>
+- The csv should follow the format <em>(first name, last name, annual salary, super rate (%), pay period)</em>
+- Start Visual Studio 2022 and select <em>Open a project or solution</em>
+- Navigate to the solution file <em>datacom_20220725\BackendCodingExercise\BackendCodingExercise.sln</em>
+- Press F5 or the play button to start debugging 
+- Enter the filename of the csv 
+- The csv should be processed and an output should be shown in console as well as an ouput file named <em>results.csv</em> will be generated
+
+---
+## Assumptions made
+- The months would always be entered by their whole name and always be correct 
+    - (January, February, March ,April, May, June, July, August, September, October, November, December)
+- An output csv file would also be wanted
+- The output file named results.csv would not exist and would not be open 
+- The output file would need column names because it may be read by human eyes, making it more readable and user friendly
+- The formatting of the csv file would be consistent
+    - Always have no spaces before and after a comma
+- The super rate would always be an integer
+- That the input csv file would not have a header for column names
+- The input csv file's format would be static
